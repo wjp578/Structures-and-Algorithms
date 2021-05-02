2c7efdf93609eafc148de97408bb436ed35f68e2
wjp578 <2923525168@qq.com>
2021-05-02 20:31:26 +0800

    5.1

--- 1.LineList/SeqList/seqList_Ope.c
+++ 1.LineList/SeqList/seqList_Ope.c
@@ -11,3 +11,28 @@ int ListLength(const SeqList* sl)
 {
     return sl->sz;
 }
+
+int ListInsert(SeqList* sl,int index,int elem)
+{
+    if(sl->sz>=MAX_LEN)
+    {
+        printf("顺序表已满\n");
+        return 0;
+    }
+
+    else if(index<1||index>sl->sz+1)
+        {
+            printf("输入位置非法\n");
+            return 0;
+        }
+
+
+    int i;
+    for(i=sl->sz-1;i>index-2;i--)
+    {
+        sl->arr[i+1]=sl->arr[i];
+    }
+    sl->arr[i+1]=elem;
+    sl->sz++;
+    return 1;
+}
--- 1.LineList/SeqList/seqList_Ope.h
+++ 1.LineList/SeqList/seqList_Ope.h
@@ -13,4 +13,8 @@ typedef struct
 void ListInitiate(SeqList* sl);
 //求长度
 int ListLength(const SeqList* sl);
+//插入数据元素
+//插入成功---1
+//插入失败---0
+int ListInsert(SeqList* sl,int index/*插入下标（位置）*/,int elem/*插入元素*/);
 
