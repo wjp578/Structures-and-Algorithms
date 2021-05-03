a02cd906e4ab52e254bb37b2387977d5b6662119
wjp578 <2923525168@qq.com>
2021-05-03 22:52:10 +0800

    5.1-5.3

--- 1.LineList/SeqList/main.c
+++ 1.LineList/SeqList/main.c
@@ -6,6 +6,29 @@ int main()
 {
     SeqList l;
     ListInitiate(&l);
-//    printf("%d",ListLength(&l));
+
+//    if(!ListInsert(&l,1,8))
+//        printf("≤Â»Î ß∞‹£°£°\n");
+//    ListInsert(&l,2,8);//≤Â»Î≤‚ ‘
+
+    int i,x;
+    while(scanf("%d%d",&i,&x)!=EOF)
+    {
+        if(!ListInsert(&l,i,x))
+            printf("≤Â»Î ß∞‹£°£°\n");
+    }
+    ListPrint(&l);
+    printf("l.size=%d\n",ListLength(&l));
+
+    ListDelete(&l,2,&x);
+    ListPrint(&l);
+    printf("delete=%d\n",x);
+    printf("l.size=%d\n",ListLength(&l));
+
+    if(!ListGet(&l,3,&x))
+        printf("ªÒ»°÷µ ß∞‹\n");
+    else
+        printf("get=%d",x);
+
     return 0;
 }
--- 1.LineList/SeqList/seqList_Ope.c
+++ 1.LineList/SeqList/seqList_Ope.c
@@ -12,6 +12,17 @@ int ListLength(const SeqList* sl)
     return sl->sz;
 }
 
+void ListPrint(const SeqList* sl)
+{
+    int i=0;
+    while(i<sl->sz)
+    {
+        printf("%d  ",sl->arr[i]);
+        i++;
+    }
+    printf("\n");
+}
+
 int ListInsert(SeqList* sl,int index,int elem)
 {
     if(sl->sz>=MAX_LEN)
@@ -34,5 +45,41 @@ int ListInsert(SeqList* sl,int index,int elem)
     }
     sl->arr[i+1]=elem;
     sl->sz++;
+    printf("successfully\n");
     return 1;
+}
+
+int ListDelete(SeqList* sl,int index,int* elem)
+{
+    if(index<1||index>sl->sz)
+    {
+        printf("输入位置非法\n");
+        return 0;
+    }
+
+    //保存被删除元素
+    *elem=sl->arr[index-1];
+
+    //删除元素
+    int i;
+    for(i=index-1;i<sl->sz-1;i++)
+        sl->arr[i]=sl->arr[i+1];
+
+    //多出来的数据无需清理，逻辑sz-1即可(逻辑删除)
+    sl->sz--;
+
+    return 1;
+}
+
+int ListGet(SeqList* sl,int index,int* elem)
+{
+    if(index<1||index>sl->sz)
+    {
+        printf("输入位置非法\n");
+        return 0;
+    }
+
+    *elem=sl->arr[index-1];
+    return 1;
+
 }
