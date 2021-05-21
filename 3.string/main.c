3274fa7e6de0b574f8471cb1e14600d409b4739b
wjp578 <2923525168@qq.com>
2021-05-22 01:36:27 +0800

    5.19~5.21

---
+++ 3.string/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "string_Ope.h"
+
+int main()
+{
+    Dstring str1,str2;
+    Dstring *string1=&str1,*string2=&str2;
+    Initiate(string1);
+    Initiate(string2);
+
+    //赋值三种情况测试
+//    Assign_Operate(string1,"");
+//    print(string1);
+
+    Assign_Operate(string1,"abdiasbd");
+    print(string1);
+
+    Assign_Operate(string1,"nnmm");
+    print(string1);
+
+    //插入测试
+    Assign_Operate(string2,"ads");
+    Insert(string1,string2,1);
+    print(string1);
+
+    //删除测试
+    Delete(string1,1,0);
+    print(string1);
+
+    //取子串测试
+    print(string2);
+    SubString(string1,2,1,string2);
+    print(string2);
+
+
+    destory(string1);
+    destory(string2);
+
+    return 0;
+}
---
+++ 3.string/string_Ope.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char *String;
+    int len;
+    int max_len;
+} Dstring;
+
+//空字符串在此处要注意
+//多个函数对空字符串有特殊处理
+
+//初始化
+void Initiate(Dstring *str);
+//将pos位置后字符后移offset(子串长度)个位置
+void String_Offset(Dstring *str, int pos, int offset);
+//字符串赋值
+void Assign_Operate(Dstring *str, const char *src);
+//打印string
+void print(const Dstring *str);
+//销毁
+void destory(Dstring *str);
+//插入子串
+int Insert(Dstring *str, const Dstring *sub_str, int pos);
+//删除子串
+int Delete(Dstring *str, int pos, int del_len);
+//取子串
+int SubString(Dstring *str, int pos, int get_len,Dstring *sub_str);
